e1cf9fcfbeb5ee4f7ddba2858916016de436995f
Eric Li <ericli@knights.ucf.edu>
2021-08-05 15:13:49 -0600

    Complete initial Agent-test

--- test/Agent-test.js
+++ test/Agent-test.js
@@ -1,7 +1,29 @@
 import { expect } from 'chai';
 import Agent from '../src/Agent';
-const data = require('../src/data/travlerTestData');
+import Traveler from '../src/Traveler';
+import Trip from '../src/Trip';
+const userData = require('../src/data/travelerTestData');
+const tripData = require('../src/data/tripTestData');
 
 describe('Agent', () => {
 
-})
+  let agent;
+  let traveler;
+  let trip;
+  // let destination;
+
+  beforeEach(() => {
+    trip = new Trip(tripData[0]);
+    traveler = new Traveler(userData[0]);
+    agent = new Agent(userData[1]);
+    traveler.trips.push(trip);
+  });
+
+  it.skip('Should be a function', () => {
+    expect(Agent).to.be.a('function');
+  });
+
+  it.skip('Should be an instance of Agent', () => {
+    expect(agent).to.be.an.instanceOf(Agent);
+  });
+});
