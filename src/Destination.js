4fd5983097cf04e21076a1c034ee9eb64f297813
Eric Li <ericli@knights.ucf.edu>
2021-08-05 15:51:38 -0600

    Complete initial Destination class

--- src/Destination.js
+++ src/Destination.js
@@ -1,1 +1,12 @@
+class Destination {
+  constructor(data) {
+    this.id = data.id;
+    this.location = data.destination;
+    this.lodgingCost = data.estimatedLodgingCostPerDay;
+    this.flightCost = data.estimatedFlightCostPerPerson;
+    this.image = data.image;
+    this.imgAltText = data.alt;
+  }
+}
+
 export default Destination;
