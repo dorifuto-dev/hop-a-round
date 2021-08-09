14a0e60ed76d3a0f2efa50263b0989618b680105
Eric Li <ericli@knights.ucf.edu>
2021-08-08 19:16:47 -0600

    Style initial default page, add slideshow

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,35 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
+var slides = document.querySelectorAll(".slide");
 
+var slideIndex = 0;
+
+
+
+const showSlides = () => {
+  // slides.forEach(slide => {
+  //   slide.style.display = "none";
+  // })
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000);
+
+}
+
+showSlides();
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
 
 // After GETting data, store in global variables
 
