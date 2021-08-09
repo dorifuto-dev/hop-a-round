04c672ea8ef44e179c3b05256dc495d80c8c1b7d
Eric Li <ericli@knights.ucf.edu>
2021-08-09 17:14:01 -0600

    Create file domFunctions.js

---
+++ src/domFunctions.js
@@ -0,0 +1,14 @@
+
+
+const domUpdateFunctions = {
+  // sample() {
+  //   console.log("HELLLOOOOO")
+  // }
+
+  clearError() {
+    const loginError = document.getElementById("loginError");
+    loginError.innerHTML = "";
+  }
+}
+
+export default domUpdateFunctions;
